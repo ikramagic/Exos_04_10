7e3d922274f9d0848066d77b9b3c222216b1b8af
ikramiste <ikrame.saadi@gmail.com>
2023-10-04 16:01:52 +0200

    first rule set in 00_hello

--- Ikrame_04/lib/00_hello.rb
+++ Ikrame_04/lib/00_hello.rb
@@ -1,2 +1,7 @@
 # 00_hello.rb
 
+def say_hello # on donne un nom à la règle à venir, qu'on pourra ensuite "invoquer" avec ce nom
+    puts "Il a dit bonjour, il a sauvé sa mère !" # on définit la règle comme étant affiche "this"
+end
+
+say_hello # on invoque l'execution de la règle en utilisant son nom comme raccourci
